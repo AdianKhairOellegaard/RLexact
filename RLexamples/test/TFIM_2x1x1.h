d70cecb6f1f6c99a7a060babcbb6f5d91c4a5f9c
AdianKhairOellegaard <adiankhair8@gmail.com>
2026-08-18 09:37:50 +0200

    Added test file TFIM 2x1x1 example.

---
+++ RLexamples/test/TFIM_2x1x1.h
@@ -0,0 +1,32 @@
+Ritz_conv 0.000001
+Zero_vec_length 0.000001
+Mode 0
+Unimode 0
+M_Symmetry 0
+Find_cross 1
+H start 0
+H end 1
+H step 0.05
+Hx 1
+Hy 0
+Hz 0
+Number of spins 2
+Number of couplings 2
+Number of coupling strengths 1
+Number of Hardcoded symmetries 0
+Number of spins in unit cell 1
+Number of Chosen GS q-values 0
+Number of custom symmetries 2
+Construct symmetries 0
+Translation indices 1 0 0
+Translation vector 1
+Qmax translation 1 1 1
+Number of dimensions 1
+Relative position 0 0 0
+Relative position 1 0 0
+Custom symmetry 0 1
+Custom symmetry 1 0
+Coupling strength vector -1 0 0
+Coupling vector 0 1 0
+Coupling vector 1 0 0
+
